f16302be1b9d09f00112acfc584d618ae30b26a3
geelenb <geelen.bram@hotmail.com>
2022-03-06 17:40:53 +0100

    Various

--- gebeurtenis_data.js
+++ gebeurtenis_data.js
@@ -1,173 +1,222 @@
 function initialize_per_player(gebeurtenis_data, value = 0) {
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    return (
-        gebeurtenis_data
-            .filter(geb => (geb['GebStatus'] === 10))
-            .reduce((acc, geb) => ({...acc, [geb['RelGUID']]: value}), {})
-    )
+	gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
+	return (
+		gebeurtenis_data
+			.filter(geb => (geb['GebStatus'] === 10))
+			.reduce((acc, geb) => ({...acc, [geb['RelGUID']]: value}), {})
+	)
 }
 
 function gebeurtenis_data_to_points(gebeurtenis_data) {
-    if (gebeurtenis_data === null) {
-        return {}
-    }
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    let points = initialize_per_player(gebeurtenis_data, 0);
-    gebeurtenis_data.forEach(geb => {
-        if (geb['GebType'] === 10) {
-            points[geb['RelGUID']] |= 0; // some games don't record in/uit
-            points[geb['RelGUID']] += parseInt(geb['Text'].split(' ')[0]);
-        }
-    });
-    return points
+	if (gebeurtenis_data === null) {
+		return {}
+	}
+	gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
+	let points = initialize_per_player(gebeurtenis_data, 0);
+	gebeurtenis_data.forEach(geb => {
+		if (geb['GebType'] === 10) {
+			points[geb['RelGUID']] |= 0; // some games don't record in/uit
+			points[geb['RelGUID']] += parseInt(geb['Text'].split(' ')[0]);
+		}
+	});
+	return points
 }
 
 function gebeurtenis_data_to_plus_minus(gebeurtenis_data) {
-    if (gebeurtenis_data === null) {
-        return {}
-    }
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    let plus_minus = initialize_per_player(gebeurtenis_data, 0);
+	if (gebeurtenis_data === null) {
+		return {}
+	}
+	gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
+	let plus_minus = initialize_per_player(gebeurtenis_data, 0);
 
-    let players_on_field = {T: [], U: []};
+	let players_on_field = {T: [], U: []};
 
-    gebeurtenis_data.forEach(geb => {
-        if (geb.GebStatus !== 10) {
-            return;
-        }
+	gebeurtenis_data.forEach(geb => {
+		if (geb.GebStatus !== 10) {
+			return;
+		}
 
-        if (geb['GebType'] === 50) {
-            // wissel
-            if (geb['Text'] === 'in') {
-                players_on_field[geb.TofU].push(geb['RelGUID']);
-            } else if (geb['Text'] === 'uit') {
-                players_on_field.T = players_on_field.T.filter(v => v !== geb['RelGUID']);
-                players_on_field.U = players_on_field.U.filter(v => v !== geb['RelGUID']);
-            }
-        } else if (geb['GebType'] === 10) {
-            // score
-            let punten = Number(geb['Text'].split(' ')[0]);
-            if (geb['TofU'] === 'U') {
-                punten = -punten;
-            }
-            for (let player of players_on_field.T) {
-                plus_minus[player] += punten
-            }
-            for (let player of players_on_field.U) {
-                plus_minus[player] -= punten
-            }
-        }
-    });
-    return plus_minus
+		if (geb['GebType'] === 50) {
+			// wissel
+			if (geb['Text'] === 'in') {
+				players_on_field[geb.TofU].push(geb['RelGUID']);
+			} else if (geb['Text'] === 'uit') {
+				players_on_field.T = players_on_field.T.filter(v => v !== geb['RelGUID']);
+				players_on_field.U = players_on_field.U.filter(v => v !== geb['RelGUID']);
+			}
+		} else if (geb['GebType'] === 10) {
+			// score
+			let punten = Number(geb['Text'].split(' ')[0]);
+			if (geb['TofU'] === 'U') {
+				punten = -punten;
+			}
+			for (let player of players_on_field.T) {
+				plus_minus[player] += punten
+			}
+			for (let player of players_on_field.U) {
+				plus_minus[player] -= punten
+			}
+		}
+	});
+	return plus_minus
 }
 
 function gebeurtenis_data_to_fouls(gebeurtenis_data) {
-    if (gebeurtenis_data === null) {
-        return {}
-    }
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    let fouls = initialize_per_player(gebeurtenis_data, '');
+	if (gebeurtenis_data === null) {
+		return {}
+	}
+	gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
+	let fouls = initialize_per_player(gebeurtenis_data, '');
 
-    for (let geb of gebeurtenis_data) {
-        if (geb['GebType'] === 30 && geb['GebStatus'] === 10) {
-            fouls[geb['RelGUID']] += geb['Text'][0]
-        }
-    }
-    const filter_special_fouls = foul_str => Array.from(foul_str).filter(p => (p !== 'P')).join('');
-    const fouls_html_representation = foul_str => `${foul_str.length}<sup>${filter_special_fouls(foul_str)}</sup>`;
+	for (let geb of gebeurtenis_data) {
+		if (geb['GebType'] === 30 && geb['GebStatus'] === 10) {
+			fouls[geb['RelGUID']] += geb['Text'][0]
+		}
+	}
+	const filter_special_fouls = foul_str => Array.from(foul_str).filter(p => (p !== 'P')).join('');
+	const fouls_html_representation = foul_str => `${foul_str.length}<sup>${filter_special_fouls(foul_str)}</sup>`;
 
-    for (let id in fouls) {
-        fouls[id] = fouls_html_representation(fouls[id])
-    }
+	for (let id in fouls) {
+		fouls[id] = fouls_html_representation(fouls[id])
+	}
 
-    return fouls
+	return fouls
 }
 
+function add_detailed_minute_to_gebnis(gebnis) {
+	const copy = [].concat(gebnis.map(geb => ({...geb})));
+	copy.forEach((geb, i) => {
+		if (i === 0) {
+			geb.index_this_minute = 0;
+			return;
+		}
+
+		const prev = copy[i - 1];
+		if (geb.Minuut !== prev.Minuut) {
+			geb.index_this_minute = 0;
+			return;
+		}
+
+		if (geb.GebStatus !== 10) {
+			geb.index_this_minute = prev.index_this_minute;
+			return
+		}
+
+		if (geb.GebType === 50 && ![10, 30].includes(prev.GebType)) {
+			// wissel, and previous was not a score, foul
+			geb.index_this_minute = prev.index_this_minute;
+			return;
+		}
+
+		geb.index_this_minute = 1 + prev.index_this_minute;
+	})
+
+	copy.forEach((geb, i) => {
+		const this_quarter = geb.Periode;
+		const this_minute = geb.Minuut;
+		const gebs_this_minute = copy.filter(geb => (geb.Periode == this_quarter) && (geb.Minuut == this_minute));
+		let len_this_minute = Math.max(...gebs_this_minute.map(geb => geb.index_this_minute))
+		if (this_minute !== 10) {
+			len_this_minute += 1;
+		}
+		geb.detailed_minute = (
+			(geb.Periode - 1) * 10 +
+			(geb.Minuut - 1) +
+			(geb.index_this_minute / len_this_minute || 0)
+		);
+	})
+
+	return copy;
+}
+
+
+function minutes_for_player(gebeurtenis_data, player_relguid) {
+	let on = false;
+	let minutes = 0;
+
+	const gebnis = add_detailed_minute_to_gebnis(gebeurtenis_data['GebNis'] || gebeurtenis_data);
+
+	gebnis.forEach((geb) => {
+		if (geb.GebStatus !== 10) {
+			return;
+		}
+		if (geb.RelGUID !== player_relguid) {
+			return;
+		}
+
+		if (geb.Text === 'in') {
+			on = true;
+			minutes -= geb.detailed_minute;
+		}
+
+		if (geb.Text === 'uit') {
+			on = false;
+			minutes += geb.detailed_minute;
+		}
+	});
+	if (on) {
+		const copy = [].concat(gebnis)
+		copy.reverse();
+		const last_geb = copy.find(geb => geb.Periode < 99);
+		minutes += last_geb.detailed_minute;
+	}
+	return minutes;
+}
+
+
 function gebeurtenis_data_to_minutes(gebeurtenis_data) {
-    if (gebeurtenis_data === null) {
-        return {}
-    }
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    let minutes = initialize_per_player(gebeurtenis_data, 0);
-    let players_on_field = [];
+	if (gebeurtenis_data === null) {
+		return {}
+	}
+	let minutes = initialize_per_player(gebeurtenis_data, 0);
 
-    for (let geb of gebeurtenis_data) {
-        if (geb['GebStatus'] === 10) {
-            if (geb['GebType'] === 40) {
-                // last 10 uit's get 1 for free to ajust for counting the minutes 1-10
-                gebeurtenis_data
-                    .filter(geb2 => geb['Index'] < geb2['Index']) // happens after
-                    .filter(geb2 => geb2['Index'] <= geb['Index'] + 10) // only 10
-                    .filter(geb2 => geb2['Text'] === 'in')
-                    .forEach(geb2 => minutes[geb2['RelGUID']] += 1)
-            }
+	Object.keys(minutes).forEach(relguid => minutes[relguid] = Math.round(minutes_for_player(gebeurtenis_data, relguid)))
 
-            if (geb['GebType'] === 50) {
-                let minuut = parseInt(geb['Minuut']);
-                if (geb['Text'] === 'in') {
-
-                    minutes[geb['RelGUID']] -= minuut;
-                    players_on_field.push(geb['RelGUID'])
-                } else if (geb['Text'] === 'uit') {
-                    minutes[geb['RelGUID']] += minuut;
-                    players_on_field = players_on_field.filter(rg => rg !== geb['RelGUID'])
-                }
-            }
-        }
-    }
-
-    // If players were not subbed off at the end of the game;
-    let last_minute = 10; // TODO: overtime periods
-    last_minute += 1;
-    for (let guid of players_on_field) {
-        minutes[guid] += last_minute
-    }
-
-    return minutes
+	return minutes
 }
 
 function gebeurtenis_data_to_free_throws(gebeurtenis_data) {
-    if (gebeurtenis_data === null) {
-        return {}
-    }
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    let result = initialize_per_player(gebeurtenis_data, 0);
+	if (gebeurtenis_data === null) {
+		return {}
+	}
+	gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
+	let result = initialize_per_player(gebeurtenis_data, 0);
 
-    const free_throws = gebeurtenis_data.filter(g => g['GebType'] === 10).filter(g => g['Text'][0] === '1');
-    for (let g of free_throws) {
-        result[g['RelGUID']] += 1
-    }
+	const free_throws = gebeurtenis_data.filter(g => g['GebType'] === 10).filter(g => g['Text'][0] === '1');
+	for (let g of free_throws) {
+		result[g['RelGUID']] += 1
+	}
 
-    return result
+	return result
 }
 
 function gebeurtenis_data_to_free_throws_attempted(gebeurtenis_data) {
-    if (gebeurtenis_data === null) {
-        return {}
-    }
+	if (gebeurtenis_data === null) {
+		return {}
+	}
 
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    const n_free_throws_from_geb = x => (Number(x['Text'][1]) || 0);
-    const fouls = gebeurtenis_data.filter(x => x['GebType'] === 30);
+	gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
+	const n_free_throws_from_geb = x => (Number(x['Text'][1]) || 0);
+	const fouls = gebeurtenis_data.filter(x => x['GebType'] === 30);
 
-    return {
-        T: fouls.filter(x => x['TofU'] === 'U').map(n_free_throws_from_geb).reduce(sum, 0),
-        U: fouls.filter(x => x['TofU'] === 'T').map(n_free_throws_from_geb).reduce(sum, 0)
-    };
+	return {
+		T: fouls.filter(x => x['TofU'] === 'U').map(n_free_throws_from_geb).reduce(sum, 0),
+		U: fouls.filter(x => x['TofU'] === 'T').map(n_free_throws_from_geb).reduce(sum, 0)
+	};
 }
 
 function gebeurtenis_data_to_three_pt(gebeurtenis_data) {
-    if (gebeurtenis_data === null) {
-        return {}
-    }
-    gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
-    let result = initialize_per_player(gebeurtenis_data, 0);
+	if (gebeurtenis_data === null) {
+		return {}
+	}
+	gebeurtenis_data = gebeurtenis_data['GebNis'] || gebeurtenis_data;
+	let result = initialize_per_player(gebeurtenis_data, 0);
 
-    const three_pt = gebeurtenis_data.filter(g => g['GebType'] === 10).filter(g => g['Text'][0] === '3');
-    for (let g of three_pt) {
-        result[g['RelGUID']] |= 0;
-        result[g['RelGUID']] += 1
-    }
+	const three_pt = gebeurtenis_data.filter(g => g['GebType'] === 10).filter(g => g['Text'][0] === '3');
+	for (let g of three_pt) {
+		result[g['RelGUID']] |= 0;
+		result[g['RelGUID']] += 1
+	}
 
-    return result
+	return result
 }
